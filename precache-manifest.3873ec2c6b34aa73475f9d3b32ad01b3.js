447058848dc6375dd62d9e60be9afc1967bd64f0
미엘9 <ynifamily3@gmail.com>
2019-01-30 21:43:37 +0900

    Updates

---
+++ precache-manifest.3873ec2c6b34aa73475f9d3b32ad01b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db62451bae078337db92",
+    "url": "/movie_app/static/css/main.8d888b3f.chunk.css"
+  },
+  {
+    "revision": "db62451bae078337db92",
+    "url": "/movie_app/static/js/main.db62451b.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "840113a53fff99d294a04d202955b892",
+    "url": "/movie_app/index.html"
+  }
+];
